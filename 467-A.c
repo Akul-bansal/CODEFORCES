bba06fa9be9eb4488b7b2ead788be8f81ee03217
Akul-bansal <akulbansal57@gmail.com>
2025-10-12 19:56:07 +0530

    Create 467-A.c

---
+++ 467-A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, p, q, count = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d %d", &p, &q);
+        if (q > p + 1)
+            count++;
+    }
+    printf("%d", count);
+    return 0;
+}
