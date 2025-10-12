71e8f4bba8441896abc1b8ae118ee7f5e202cfb7
Akul-bansal <akulbansal57@gmail.com>
2025-10-12 19:57:32 +0530

    Update 467-A.c

--- 467-A.c
+++ 467-A.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main()
-{
+{                                             // Problem 2070A - George and Accommodation
     int n, p, q, count = 0;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++)
