e7f10d4dc76048841203c298d4eaab0b9e7e50d8
Akul-bansal <akulbansal57@gmail.com>
2025-10-18 17:40:50 +0530

    Create 110-A.c

---
+++ 110-A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+                                                             
+int main()
+{
+    long long int n;
+    int t, count = 0;
+    scanf("%lld", &n);
+
+    if (n != 0)
+    {
+        while (n != 0)
+        {
+            t = n % 10;
+            if (t == 4 || t == 7)
+            {
+                count++;
+            }
+            n = n / 10;
+        }
+
+        if (count == 4 || count == 7)
+            printf("YES");
+        else
+            printf("NO");
+    }
+    else
+        printf("NO");
+    return 0;
+}
