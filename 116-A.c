11e1ecb5ccd736cb17f37e22665d891047a4d00e
Akul-bansal <akulbansal57@gmail.com>
2025-10-19 18:38:28 +0530

    Create 116-A.c

---
+++ 116-A.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, capacity = 0;
+    scanf("%d", &n);
+    int arr[n][2];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int minimum_capacity = 0;
+    for (int i = 0; i < n; i++)
+    {
+        capacity = capacity + arr[i][1];
+        capacity = capacity - arr[i][0];
+
+        if (minimum_capacity < capacity)
+            minimum_capacity = capacity;
+    }
+    printf("%d", minimum_capacity);
+    return 0;
+}
