921e5fce7ee176dd679a726701e4b18575543417
Akul-bansal <akulbansal57@gmail.com>
2025-10-20 12:31:10 +0530

    Create 1409-A.c

---
+++ 1409-A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    long int n;
+    long long int a, b, t;
+    scanf("%ld", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%lld %lld", &a, &b);
+        if (a == b)
+        {
+            t = 0;
+        }
+        else if (a < b)
+        {
+            t = b - a;
+        }
+        else
+            t = a - b;
+        long long int c;
+        c = t / 10;
+        if (t % 10 == 0)
+            printf("%lld\n", c);
+        else
+            printf("%lld\n", c + 1);
+    }
+    return 0;
+}
