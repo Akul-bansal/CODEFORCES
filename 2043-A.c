05a577eb330f9fefe6e446ed62c243692c85b4a2
Akul-bansal <akulbansal57@gmail.com>
2025-10-21 19:11:38 +0530

    Create 2043-A.c

---
+++ 2043-A.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    long long int a;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%lld", &a);
+        long long int count = 1;
+        if (a <= 3)
+            printf("1\n");
+        else
+        {
+            while (a > 3)
+            {
+                a = a / 4;
+                count = count * 2;
+            }
+            printf("%lld\n", count);
+        }
+    }
+    return 0;
+}
