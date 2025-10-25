fe862d6b0c2f3d934c7cb4fa01f8b57b20f15cec
Akul-bansal <akulbansal57@gmail.com>
2025-10-25 18:59:09 +0530

    Create 1296A.c

---
+++ 1296A.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, t;
+    scanf("%d", &n);
+    for (int j = 1; j <= n; j++)
+    {
+        scanf("%d", &t);
+        int arr[t];
+
+        for (int i = 0; i < t; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+        int count = 0;
+
+        for (int i = 0; i < t; i++)
+        {
+
+            if (arr[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+        if (t % 2 == 0)
+        {
+            if (count == 0 || count == t)
+                printf("NO\n");
+            else
+                printf("YES\n");
+        }
+        else
+        {
+            if (count == t)
+                printf("NO\n");
+            else
+                printf("YES\n");
+        }
+    }
+    return 0;
+}
