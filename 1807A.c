a563921244114dd221c7d54dbcf54de361d0e337
Akul-bansal <akulbansal57@gmail.com>
2025-10-26 17:20:01 +0530

    Create 1807A.c

---
+++ 1807A.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a, b, c;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        if (a + b == c)
+            printf("+\n");
+        else
+            printf("-\n");
+    }
+    return 0;
+}
