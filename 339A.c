16249f822db3bf1f18bd2c5117e85cdde2e73519
Akul-bansal <akulbansal57@gmail.com>
2025-10-27 22:38:26 +0530

    Create 339A.c

---
+++ 339A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[1000];
+    char temp;
+    scanf("%[^\n]", str);
+    int length = strlen(str);
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - 2; j += 2)
+        {
+            if (str[j] > str[j + 2])
+            {
+                temp = str[j];
+                str[j] = str[j + 2];
+                str[j + 2] = temp;
+            }
+        }
+    }
+    printf("%s\n", str);
+
+    return 0;
+}
