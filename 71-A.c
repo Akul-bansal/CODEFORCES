89032cfe9ba4b0fadcacb7931b1831c2b8bbe852
Akul-bansal <akulbansal57@gmail.com>
2025-10-31 16:47:44 +0530

    Create 71-A.c

---
+++ 71-A.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        char str[100];
+        scanf("%s", str);
+        int length = strlen(str);
+        char a = str[0];
+        char b = str[length - 1];
+        if (length < 10)
+            printf("%s\n", str);
+        else
+            printf("%c%d%c\n", a, length - 2, b);
+    }
+
+    return 0;
+}
