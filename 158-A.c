a0b94f16762a2e0b76cddaf57151d613f9b588f3
Akul-bansal <akulbansal57@gmail.com>
2025-11-01 17:59:40 +0530

    Create 158-A.c

---
+++ 158-A.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k;
+    scanf("%d %d", &n, &k);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int count = 0;
+    int t = arr[k - 1];
+    if (t != 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] >= t)
+            {
+                count++;
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] > t)
+            {
+                count++;
+            }
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
