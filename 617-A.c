574eb49691e289cbe2f520cfa2935a709ddb9d62
Akul-bansal <akulbansal57@gmail.com>
2025-11-02 19:11:18 +0530

    Create 617-A.c

---
+++ 617-A.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, count = 0;
+    scanf("%d", &x);
+    while (x > 0)
+    {
+        if (x >= 5)
+        {
+            count++;
+            x -= 5;
+        }
+        else if (x == 4)
+        {
+            count++;
+            x -= 4;
+        }
+        else if (x == 3)
+        {
+            count++;
+            x -= 3;
+        }
+        else if (x == 2)
+        {
+            count++;
+            x -= 2;
+        }
+        else
+        {
+            count++;
+            x -= 1;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
