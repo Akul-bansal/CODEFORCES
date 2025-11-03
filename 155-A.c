130606d94f3a93f1464237cda07014ae726e089a
Akul-bansal <akulbansal57@gmail.com>
2025-11-03 10:34:06 +0530

    Create 155-A.c

---
+++ 155-A.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int count = 0;
+    int max = arr[0], min = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (max < arr[i])
+        {
+            max = arr[i];
+            count++;
+        }
+        if (min > arr[i])
+        {
+            min = arr[i];
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
