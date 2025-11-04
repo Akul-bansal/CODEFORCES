bf4b2e32c4560d9d234758b2c347a05916bf2df1
Akul-bansal <akulbansal57@gmail.com>
2025-11-04 21:27:27 +0530

    Create 148-A.c

---
+++ 148-A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int k, l, m, n;
+    long int d;
+    scanf("%d", &k);
+    scanf("%d", &l);
+    scanf("%d", &m);
+    scanf("%d", &n);
+    scanf("%ld", &d);
+    int count = 0;
+    for (long int i = 1; i <= d; i++)
+    {
+
+        if (i % n == 0 || i % k == 0 || i % l == 0 || i % m == 0)
+            count++;
+    }
+    printf("%d", count);
+    return 0;
+}
