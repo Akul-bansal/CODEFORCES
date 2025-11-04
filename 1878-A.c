5fd97e456e5f22a14d20a296545b940783e05a91
Akul-bansal <akulbansal57@gmail.com>
2025-11-04 21:47:52 +0530

    Create 1878-A.c

---
+++ 1878-A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, n, k;
+    scanf("%d", &a);
+    for (int i = 1; i <= a; i++)
+    {
+        scanf("%d %d", &n, &k);
+        int arr[n];
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &arr[j]);
+            if (k == arr[j])
+                count++;
+        }
+        if (count > 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+    return 0;
+}
