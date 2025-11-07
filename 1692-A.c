884729c6a5b4fdd12dbc28476519d8fcdcd7ad7a
Akul-bansal <akulbansal57@gmail.com>
2025-11-08 04:07:39 +0530

    Create 1692-A.c

---
+++ 1692-A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int a, b, c, d;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        int count = 0;
+        scanf("%d %d %d %d", &a, &b, &c, &d);
+        if (a < b)
+            count++;
+        if (a < c)
+            count++;
+        if (a < d)
+            count++;
+        printf("%d\n", count);
+    }
+    return 0;
+}
