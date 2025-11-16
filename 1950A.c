8038ed5f43e32e79d2d00bd6446c5123fd8f5777
Akul-bansal <akulbansal57@gmail.com>
2025-11-16 21:59:30 +0530

    Create 1950A.c

---
+++ 1950A.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a,b,c;
+    for(int i=0;i<n;i++){
+        scanf("%d %d %d",&a,&b,&c);
+
+        if(c>b && b>a){
+            printf("STAIR\n");  
+        }
+        else if(b>a && b>c){
+            printf("PEAK\n");
+        }
+        else{
+           
+            printf("NONE\n");
+
+
+        }
+    }
+    return 0;
+}
