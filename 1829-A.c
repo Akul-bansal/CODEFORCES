634d370cc4ff46b80bd863eec289fe19616a54f9
Akul-bansal <akulbansal57@gmail.com>
2025-11-28 19:46:01 +0530

    Create 1829-A.c

---
+++ 1829-A.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        char str[11];
+        scanf("%s", str);
+        char str1[] = "codeforces";
+        int count = 0;
+        for (int j = 0; j < 11; j++)
+        {
+            if (str[j] != str1[j])
+                count++;
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
