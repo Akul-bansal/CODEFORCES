0d950caefefcc89718861537c066053e15c5157e
Akul-bansal <akulbansal57@gmail.com>
2025-12-12 18:06:57 +0530

    Create 236-A.c

---
+++ 236-A.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[110];
+    int freq[26] = {0}, count = 0;
+
+    scanf("%s", s);
+
+    for(int i = 0; s[i]; i++)
+        freq[s[i] - 'a'] = 1;
+
+    for(int i = 0; i < 26; i++)
+        count += freq[i];
+
+    if (count % 2 == 1)
+        printf("IGNORE HIM!");
+    else
+        printf("CHAT WITH HER!");
+
+    return 0;
+}
