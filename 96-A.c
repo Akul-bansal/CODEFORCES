dc397ebb1c67fdec5d0095cef6b4c53c5ffd2295
Akul-bansal <akulbansal57@gmail.com>
2025-12-13 20:41:26 +0530

    Create 96-A.c

---
+++ 96-A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[105];
+    scanf("%s", s);
+
+    int count = 1;
+    for (int i = 1; s[i]; i++) {
+        if (s[i] == s[i - 1]) {
+            count++;
+            if (count == 7) {
+                printf("YES");
+                return 0;
+            }
+        } else {
+            count = 1;
+        }
+    }
+
+    printf("NO");
+    return 0;
+}
