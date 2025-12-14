f0cfe74049f294e379ad256923becaf532beed11
Akul-bansal <akulbansal57@gmail.com>
2025-12-14 19:42:14 +0530

    Create 41-A.c

---
+++ 41-A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[110], b[110];
+    scanf("%s %s", a, b);
+
+    int len = strlen(a);
+    int len1=strlen(b);
+  
+if(len!=len1)
+  printf("NO");
+  else{
+    for(int i = 0; i < len; i++) {
+        if (a[i] != b[len - i - 1]) {
+            printf("NO");
+            return 0;
+        }
+    }
+
+    printf("YES");}
+    return 0;
+}
