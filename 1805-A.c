6f3a247dece9322ba5f16c951cec5bf21e19e422
Akul-bansal <akulbansal57@gmail.com>
2025-12-24 21:32:15 +0530

    Create 1805-A.c

---
+++ 1805-A.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        int n;
+        scanf("%d",&n);
+        int arr[n];
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[j]);
+            
+        }
+        
+        int x=arr[0];
+         for(int j=0;j<n-1;j++){
+            x=x^arr[j+1];
+            
+        }
+        if(n%2!=0)
+        printf("%d\n",x);
+        else{
+            if(x==0)
+        printf("0\n");
+            else
+        printf("-1\n");
+
+        }
+    }
+    return 0;
+}
