ef68b8c7fde1e28bdd9971b2a77c0dc094ee2ec8
Akul-bansal <akulbansal57@gmail.com>
2025-12-25 22:12:38 +0530

    Create 1829-B.c

---
+++ 1829-B.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int k=0;k<t;k++){
+        int n;
+    scanf("%d",&n);
+    int arr[n],tot=0;
+    for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    tot+=arr[i];
+    }
+    int count=1,max=1;
+    
+    for(int i=0;i<n-1;i++){
+
+        if(arr[i]+arr[i+1]==0){
+            count++;
+            if(max<count)
+            max=count;
+        }
+        else
+        count=1;
+    }
+    if(tot!=n)
+    printf("%d\n",max);
+    else
+    printf("0\n");
+    }
+    return 0;
+}
