45ac38b565307f130bd906acabe578269f731970
Akul-bansal <akulbansal57@gmail.com>
2026-01-01 02:18:13 +0530

    Create 1789A.cpp

---
+++ 1789A.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include <numeric>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int k=0;k<t;k++){
+        int n;
+        cin>>n;
+       int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int gcd=0;
+        bool result=false;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+              if(__gcd(arr[i], arr[j]) <= 2)
+                result=true;
+ 
+        }
+    
+        }
+        if(result==true)
+        cout<<"YES\n";
+        else
+        cout<<"NO\n";
+        
+    }
+    return 0;
+}
