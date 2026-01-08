64d8d4ec7db09755f22ba49ced164c96bb835a19
Akul-bansal <akulbansal57@gmail.com>
2026-01-08 16:59:59 +0530

    Create 1374-B.cpp

---
+++ 1374-B.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin>>n;
+        int count=0;
+        while(n>=6){
+            if(n%6==0){
+            n=n/6;
+            count++;
+            }
+            else{
+                n*=2;
+                count++;
+            }
+        }
+        if(n==6)
+        cout<<count+1<<endl;
+        else if(n==1)
+        cout<<count<<endl;
+        else if(n==3)
+        cout<<count+2<<endl;
+        else
+        cout<<"-1\n";
+
+    }
+    return 0;
+}
