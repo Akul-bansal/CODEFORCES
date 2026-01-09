ad524d7689426d94bde11a49e026ed5a371f9daf
Akul-bansal <akulbansal57@gmail.com>
2026-01-09 11:03:24 +0530

    Create 1440B.cpp

---
+++ 1440B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        cin>>n>>k;
+        int b=n*k;
+        int arr[b];
+        for(int i=0;i<b;i++)
+        cin>>arr[i];
+        int count=0,sum=0;
+        int a=n/2+1;
+        
+            while(count!=k){
+                sum+=arr[b-a];
+                count++;
+                a+=(n/2+1);
+            }
+        
+        cout<<sum<<endl;
+    }
+    return 0;
+}
