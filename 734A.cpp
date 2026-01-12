a2491bce69625dfe2e6940c7f7887fa1ad78bed4
Akul-bansal <akulbansal57@gmail.com>
2026-01-13 00:13:43 +0530

    Create 734A.cpp

---
+++ 734A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int A = 0, D = 0;
+    for (char c : s) {
+        if (c == 'A') A++;
+        else D++;
+    }
+
+    if (A > D)
+        cout << "Anton";
+    else if (D > A)
+        cout << "Danik";
+    else
+        cout << "Friendship";
+
+    return 0;
+}
