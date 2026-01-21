4d85cf8dbe97a275446dea88f0eb4d2c08f0bb60
Akul-bansal <akulbansal57@gmail.com>
2026-01-21 17:52:35 +0530

    Create 469A.cpp

---
+++ 469A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<bool> visited(n + 1, false);
+
+    int p;
+    cin >> p;
+    for (int i = 0; i < p; i++) {
+        int x;
+        cin >> x;
+        visited[x] = true;
+    }
+
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int x;
+        cin >> x;
+        visited[x] = true;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            cout << "Oh, my keyboard!";
+            return 0;
+        }
+    }
+
+    cout << "I become the guy.";
+    return 0;
+}
