327dd878009723b2cfe0bea5088c06b802476b88
Akul-bansal <akulbansal57@gmail.com>
2026-01-26 19:02:52 +0530

    Add solution for parity correction in array elements
    Implement a solution for parity mismatch in an array.

---
+++ 1367B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        int c0 = 0, c1 = 0;
+        for (int i = 0; i < n; i++) {
+            if ((i % 2) != (a[i] % 2)) {
+                if (i % 2 == 0) c0++;   // even index, wrong parity
+                else c1++;            // odd index, wrong parity
+            }
+        }
+
+        if (c0 != c1) cout << -1 << "\n";
+        else cout << c0 << "\n";
+    }
+    return 0;
+}
