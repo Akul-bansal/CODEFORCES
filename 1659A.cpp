967c5dce6c3940bda8e2af4f5e56635b19102a09
Akul-bansal <akulbansal57@gmail.com>
2026-01-31 02:44:54 +0530

    Create 1659A.cpp

---
+++ 1659A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,a,b;
+        string s;
+        cin >> n >> a >> b;
+        int group=b+1;
+        int atime=a/(b+1);
+        int rema=a%(b+1);
+        for(int i=1;i<=b+1;i++){
+            for(int j=1;j<=atime;j++){
+                s+='R';
+            }
+            if(rema>0){
+                s+='R';
+                rema--;
+            }
+            if(i!=b+1){
+                s+='B';
+            }
+        }
+        cout<<s<<endl;
+    }
+    return 0;
+}
