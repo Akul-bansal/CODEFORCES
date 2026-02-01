767286260ab3b5d9af2946447c768e3a30353ea5
Akul-bansal <akulbansal57@gmail.com>
2026-02-02 01:14:43 +0530

    Implement logic to compare and reduce two numbers

---
+++ 1362A.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int a,b,count=0;
+        cin>>a>>b;
+        if(a==b){
+            cout<<0<<endl;
+            
+        }
+         else{
+        if(a>b){
+            while(a!=b){
+                if(a%2==0){
+                   a=a/2;
+                    count++;
+                }
+                else
+                break;
+            }
+        }
+        else{
+            while(a!=b){
+                if(b%2==0){
+                    b=b/2;
+                    count++;
+                }
+                else
+                break;
+            }
+        }
+       if(count%3==0 && count!=0 && a==b){
+           cout<<count/3<<endl;
+       }
+       else if(count%3==1 && a==b){
+           cout<<count/3+1<<endl;
+       }
+       else if(count%3==2 && a==b){
+           cout<<count/3+1<<endl;
+        
+       }
+       else{
+           cout<<-1<<endl;
+    }
+}
+}
+    return 0;
+}
