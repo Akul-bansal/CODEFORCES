0b887df10a14eac9f0623d185f91e7a718750ccc
Akul-bansal <akulbansal57@gmail.com>
2026-02-05 21:10:39 +0530

    Create 2185A.cpp

---
+++ 2185A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+
+        for(int j=1;j<=n;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
