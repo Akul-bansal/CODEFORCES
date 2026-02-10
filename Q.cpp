aa5aef515f80d5fe9ccc172fa6874bbdfc5b859c
Akul-bansal <akulbansal57@gmail.com>
2026-02-10 22:00:53 +0530

    Create Q.cpp

---
+++ Q.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x[4], y[4];
+
+        for (int i = 0; i < 4; i++) {
+            cin >> x[i] >> y[i];
+        }
+
+        int side = 0;
+
+        // find side using equal x or y
+        for (int i = 0; i < 4; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                if (x[i] == x[j]) {
+                    side = abs(y[i] - y[j]);
+                }
+                if (y[i] == y[j]) {
+                    side = abs(x[i] - x[j]);
+                }
+            }
+        }
+
+        cout << side * side << endl;
+    }
+
+    return 0;
+}
