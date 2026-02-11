905a19a9ac8fd3f812213e3f03b296f88ae39cb3
Akul-bansal <akulbansal57@gmail.com>
2026-02-11 22:20:16 +0530

    Create 1367A.cpp

---
+++ 1367A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        string s;
+        cin >> n >> s;
+
+        string res = "";
+        res += s[0];
+
+        for (int i = 1; i < n; i += 2) {
+            res += s[i];
+        }
+
+        cout << res << endl;
+    }
+    return 0;
+}
