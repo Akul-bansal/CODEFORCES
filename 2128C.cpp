9b80e4d09b5f23c5cc51e156263eb266fa0c4b9d
Akul-bansal <akulbansal57@gmail.com>
2026-02-12 21:56:02 +0530

    Create 2128C.cpp

---
+++ 2128C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        long long arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        long long mn = arr[0];
+        bool fact = true;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] >= 2 * mn) {
+                fact = false;
+                break;
+            }
+            if (arr[i] < mn)
+                mn = arr[i];
+        }
+
+        if (fact)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
