c26d6da0f093e9f0403f6934cbdefeb214593bf2
Akul-bansal <akulbansal57@gmail.com>
2026-02-15 17:21:54 +0530

    Create 451B.cpp

---
+++ 451B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> arr(n), b(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        b[i] = arr[i];
+    }
+
+    sort(b.begin(), b.end());
+
+    if (is_sorted(arr.begin(), arr.end())) {
+        cout << "yes\n1 1\n";
+        return 0;
+    }
+
+    int l = 0, r = n - 1;
+
+    while (l < n && arr[l] == b[l]) l++;
+
+    while (r >= 0 && arr[r] == b[r]) r--;
+
+    reverse(arr.begin() + l, arr.begin() + r + 1);
+
+    if (is_sorted(arr.begin(), arr.end())) {
+        cout << "yes\n" << l + 1 << " " << r + 1 << endl;
+    } else {
+        cout << "no\n";
+    }
+
+    return 0;
+}
