4e3fe3000aa0ab5ce58933ed67f22d880331984c
Akul-bansal <akulbansal57@gmail.com>
2026-02-16 20:41:22 +0530

    Create 427A.cpp

---
+++ 427A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int x;
+    int police = 0;
+    int untreated = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+
+        if (x == -1) {
+            if (police > 0) {
+                police--;
+            } else {
+                untreated++;
+            }
+        } else {
+            police += x;
+        }
+    }
+
+    cout << untreated;
+    return 0;
+}
