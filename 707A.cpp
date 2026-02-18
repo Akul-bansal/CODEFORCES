d36b357aa5469e6b6a6e045388f08624763d8a1d
Akul-bansal <akulbansal57@gmail.com>
2026-02-18 16:46:19 +0530

    Create 707A.cpp

---
+++ 707A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    char pixel;
+    bool color = false;
+
+    for (int i = 0; i < n * m; i++) {
+        cin >> pixel;
+        if (pixel == 'C' || pixel == 'M' || pixel == 'Y') {
+            color = true;
+        }
+    }
+
+    if (color) cout << "#Color";
+    else cout << "#Black&White";
+
+    return 0;
+}
