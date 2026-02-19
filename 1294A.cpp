b3cefa06c5c0252b027ce2e2f1f5d9b87ac7b6a8
Akul-bansal <akulbansal57@gmail.com>
2026-02-19 19:33:35 +0530

    Create 1294A.cpp

---
+++ 1294A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b, c, n;
+        cin >> a >> b >> c >> n;
+
+        long long mx = max(a, max(b, c));
+        long long required = (mx - a) + (mx - b) + (mx - c);
+
+        if (required > n) {
+            cout << "NO\n";
+        }
+        else {
+            long long remaining = n - required;
+            if (remaining % 3 == 0)
+                cout << "YES\n";
+            else
+                cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
