64d66c3139675316c2a0afd3f9687bb60b7c8448
Akul-bansal <akulbansal57@gmail.com>
2026-02-20 19:11:24 +0530

    Create 1960C.cpp

---
+++ 1960C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;        
+    if (n == 2) return true;         
+    if (n % 2 == 0) return false;     
+    
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+       long long int n, m;
+        cin >> n >> m;
+        
+        
+    if (isPrime(n) && m==1)
+        cout << "YES\n";
+        else if(n==1 && m==2)
+        cout << "YES\n";
+         
+    else
+        cout << "NO\n";
+}
+return 0;
+}
