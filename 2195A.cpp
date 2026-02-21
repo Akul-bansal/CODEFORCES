bfee999cdc8f1c1e32268740dbf70b98936c67c5
Akul-bansal <akulbansal57@gmail.com>
2026-02-22 01:16:52 +0530

    Create 2195A.cpp

---
+++ 2195A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        unsigned int n;
+        cin>>n;
+        unsigned int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        int count1=0,count2=0;
+        for(int i=0;i<n;i++){
+            
+             if(arr[i]==67)
+               count2++;
+        }
+        if(count2>0)
+           cout << "Yes\n";
+        else
+           cout << "No\n";
+    }
+    return 0;
+}
