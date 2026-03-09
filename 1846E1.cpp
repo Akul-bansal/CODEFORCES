285e7859e26329718343424840e80e831decaff0
Akul-bansal <akulbansal57@gmail.com>
2026-03-09 17:47:49 +0530

    Create 1846E1.cpp

---
+++ 1846E1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+
+        bool ok = false;
+
+        for (long long k = 2; k * k <= n; k++) {
+            long long sum = 1 + k;
+            long long term = k;
+
+            while (sum < n) {
+                term *= k;
+                sum += term;
+            }
+
+            if (sum == n) {
+                ok = true;
+                break;
+            }
+        }
+
+        if (ok) cout << "YES\n";
+        else cout << "NO\n";
+    }
+}
