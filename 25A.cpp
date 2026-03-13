a49ae4b53a38d3352698847ccfbb2f224a6c98af
Akul-bansal <akulbansal57@gmail.com>
2026-03-13 11:40:59 +0530

    Create 25A.cpp

---
+++ 25A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int even=0;
+    int odd=0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] % 2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+        if(even == 2 || odd==2)
+        break;
+
+    }
+    if(even>odd){
+        for(int i = 0; i < n; i++) {
+            if (a[i] % 2 != 0) {
+                cout << i+1 << endl;
+                break;
+            }
+        }
+    }
+    else{
+        for(int i = 0; i < n; i++) {
+            if (a[i] % 2 == 0) {
+                cout << i+1 << endl;
+                break;
+            }
+        }
+    }
+   
+    return 0;
+}
