1c14ca89f99770beda1e04d413bac3b1a72add91
Akul-bansal <akulbansal57@gmail.com>
2026-03-22 19:08:10 +0530

    Create 2209A.cpp

---
+++ 2209A.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n,c,k;
+        cin>>n>>c>>k;
+        vector<long long int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        for(int i=0;i<n;i++){
+            if(a[i]<=c){
+                if(c-a[i]<=k){
+                    k=k-c+a[i];
+                    c=c+c;
+                }
+                else{
+                    c=c+k+a[i];
+                    k=0;
+                }
+            }
+            else{
+                break;
+            }
+           
+    }
+    cout << c << endl;
+}
+    return 0;
+}
