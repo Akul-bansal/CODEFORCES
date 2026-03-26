89f45daea70e1ce70256ed9669e7bb9b0fb797d9
Akul-bansal <akulbansal57@gmail.com>
2026-03-27 00:49:10 +0530

    Create 2144A.cpp

---
+++ 2144A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin>>s;
+        int num=0;
+        int t=1000;
+        for(int i=0;i<s.size();i++){
+        num+=(s[i]-'0')*t;
+        t=t/10;   
+        }
+        int st=sqrt(num);
+        if(st*st==num && st!=0){
+            cout << "1 "<<st-1<<endl;
+        }
+        else if(st==0)
+            cout << "0 0\n";
+        else
+            cout << "-1\n";
+    }
+    return 0;
+}
