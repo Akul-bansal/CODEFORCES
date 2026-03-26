e4945ebf3bee3a59413913b41f9a86a23d1e8b7a
Akul-bansal <akulbansal57@gmail.com>
2026-03-26 23:22:45 +0530

    Create 9A.cpp

---
+++ 9A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int y, w;
+    cin >> y >> w;
+
+    int mx = max(y, w);
+    int fav = 6 - mx + 1;
+
+    int g = __gcd(fav, 6);
+
+    cout << fav / g << "/" << 6 / g;
+
+    return 0;
+}
