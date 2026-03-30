4df0cb5d12285199fbe74365cc0abd4617c35893
Akul-bansal <akulbansal57@gmail.com>
2026-03-30 19:24:18 +0530

    Create 2210B.cpp

---
+++ 2210B.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin>>n;
+        long int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+       long int mint = 9999999;
+        int count=0;
+        for(int i=0;i<n;i++){
+            
+            if(i+1>=arr[i] )
+            count++;
+            
+        }
+        cout<<count<<endl;
+        
+    }
+    return 0;
+}
