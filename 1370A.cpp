55413a038609e45625eab5cdc5023a226aea2f2b
Akul-bansal <akulbansal57@gmail.com>
2026-04-08 18:41:01 +0530

    Create 1370A.cpp

---
+++ 1370A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> //Problem-
+using namespace std;
+ 
+int main()
+{
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+        long long n, i;
+        cin >> n;
+        cout<<(n/2)<<endl;
+    }
+    return 0;
+}
